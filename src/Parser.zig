8a022640ac8bd0a248a7fb9508e0da3133e8a1af
doccaico <hassy.luna@gmail.com>
2024-04-02 18:05:34 +0900

    途中: 3. Lexer - add test_add

--- src/Parser.zig
+++ src/Parser.zig
@@ -6,16 +6,44 @@ const Object = @import("Object.zig");
 pub fn parse(allocator: std.mem.Allocator, program: []const u8) !Object.Object {
     var token_result = try Lexer.tokenize(allocator, program);
 
-    const cloned = try token_result.clone();
+    var cloned = try token_result.clone();
     std.mem.reverse(Lexer.Token, cloned.items[0..]);
-    const parsed_list = parse_list(cloned);
+    const parsed_list = parse_list(allocator, &cloned);
 
     return parsed_list;
 }
 
-fn parse_list(tokens: std.ArrayList(Lexer.Token)) Object.Object {
-    _ = tokens;
-    return Object.Object{ .Integer = Object.Integer{ .value = 1 } };
+fn parse_list(allocator: std.mem.Allocator, tokens: *std.ArrayList(Lexer.Token)) !Object.Object {
+    const t = tokens.pop();
+    switch (t) {
+        .LParen => {},
+        else => {
+            return error.ExpectedLParen;
+        },
+    }
+
+    var list = std.ArrayList(Object.Object).init(allocator);
+    while (tokens.items.len != 0) {
+        const token = tokens.popOrNull() orelse return error.NotEnoughTokens;
+        switch (token) {
+            .Keyword => |x| try list.append(Object.Object{ .Keyword = Object.Keyword{ .value = x.value } }),
+            .If => try list.append(Object.Object{ .If = .{} }),
+            .BinaryOp => |x| try list.append(Object.Object{ .BinaryOp = Object.BinaryOp{ .value = x.value } }),
+            .Integer => |x| try list.append(Object.Object{ .Integer = Object.Integer{ .value = x.value } }),
+            .Float => |x| try list.append(Object.Object{ .Float = Object.Float{ .value = x.value } }),
+            .String => |x| try list.append(Object.Object{ .String = Object.String{ .value = x.value } }),
+            .Symbol => |x| try list.append(Object.Object{ .Symbol = Object.Symbol{ .value = x.value } }),
+            .LParen => {
+                try tokens.append(Lexer.Token{ .LParen = .{} });
+                const sub_list = try parse_list(allocator, tokens);
+                try list.append(sub_list);
+            },
+            .RParen => {
+                return Object.Object{ .List = Object.List{ .list = list } };
+            },
+        }
+    }
+    return Object.Object{ .List = Object.List{ .list = list } };
 }
 
 test "test_add" {
@@ -30,10 +58,7 @@ test "test_add" {
     try a1.append(.{ .Integer = .{ .value = 1 } });
     try a1.append(.{ .Integer = .{ .value = 2 } });
 
-    const expected = [_]Object.Object{
-        // .{ .List = a1 },
-        .{ .List = .{ .list = a1 } },
-    };
+    const expected = Object.Object{ .List = Object.List{ .list = a1 } };
 
-    try std.testing.expectEqualDeep(expected[0..], tokenized.List.list.items);
+    try std.testing.expectEqualDeep(expected, tokenized);
 }
